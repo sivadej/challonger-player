64b53f14cfc994e02497130316660ce3a4546d2a
Bomby K <bomby@flyntlok.com>
2022-03-19 21:39:40 -0800

    fun with placeholders

--- src/components/player-list/Players.tsx
+++ src/components/player-list/Players.tsx
@@ -1,18 +1,18 @@
-import React, { useContext } from 'react';
-import { Badge, Row, Col, Placeholder } from 'react-bootstrap';
-import usePlayersSetQuery from '@hooks/query/usePlayersSetQuery';
-import { AppContext } from '@contexts/AppContext';
-import { PlayersSet } from 'interfaces';
+import React, { useContext } from "react";
+import { Badge, Row, Col, Placeholder } from "react-bootstrap";
+import usePlayersSetQuery from "@hooks/query/usePlayersSetQuery";
+import { AppContext } from "@contexts/AppContext";
+import { PlayersSet } from "interfaces";
 
 const emptyArr: string[] = [];
 const emptyObj: PlayersSet = {};
 
 const getTournamentShortName = (tId: string): string => {
-  if (tId === '10838641') return 'ST';
-  if (tId === '10838636') return 'BBCF';
-  if (tId === '10838634') return 'GGS';
-  if (tId === '10838653') return 'MVC3';
-  return '???';
+  if (tId === "10838641") return "ST";
+  if (tId === "10838636") return "BBCF";
+  if (tId === "10838634") return "GGS";
+  if (tId === "10838653") return "MVC3";
+  return "???";
 };
 
 export default function Players(): JSX.Element {
@@ -26,18 +26,32 @@ export default function Players(): JSX.Element {
   const { names = emptyArr, entities = emptyObj } = data ?? {};
 
   const handleClick = (name: string) => {
-    dispatch({ type: 'SHOW_PLAYER_VIEW', payload: { playerName: name } });
+    dispatch({ type: "SHOW_PLAYER_VIEW", payload: { playerName: name } });
   };
 
   if (isLoading) {
     return (
       <>
-        <Placeholder as='p' animation='glow'>
-          <Placeholder xs={12} />
-        </Placeholder>
-        <Placeholder as='p' animation='glow'>
-          <Placeholder xs={12} />
-        </Placeholder>
+        <div className="d-flex justify-content-center mt-3">
+          <div
+            className="bg-dark border border-light p-3"
+            style={{ width: "85%" }}
+          >
+            <Row>
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+              <PlaceholderItem />
+            </Row>
+          </div>
+        </div>
       </>
     );
   }
@@ -45,36 +59,38 @@ export default function Players(): JSX.Element {
   return (
     <>
       {/* {JSON.stringify(state, null, 2)} */}
-      <div className='d-flex justify-content-center mt-3'>
+      <div className="d-flex justify-content-center mt-3">
         <div
-          className='bg-dark border border-light p-3'
-          style={{ width: '85%' }}
+          className="bg-dark border border-light p-3"
+          style={{ width: "85%" }}
         >
           <Row>
             {names.map((p) => {
               return (
-                  <Col xs={6} sm={4} key={p}>
-                    <div
-                      className='bg-black border border-secondary text-center p-2 m-2'
-                      role='button'
-                      onClick={() => handleClick(p)}
-                    >
-                      <div style={{ fontWeight: 'bold', fontSize: '1.4rem' }}>
-                        {p.toUpperCase()}
-                      </div>
-                      <div
-                        style={{
-                          display: 'flex',
-                          gap: '0.5rem',
-                          justifyContent: 'center',
-                        }}
-                      >
-                        {entities[p]?.map((pl) => (
-                          <Badge key={pl.tournamentId}>{getTournamentShortName(pl.tournamentId)}</Badge>
-                        ))}
-                      </div>
+                <Col xs={12} sm={6} md={4} key={p}>
+                  <div
+                    className="bg-black border border-secondary text-center p-2 m-2"
+                    role="button"
+                    onClick={() => handleClick(p)}
+                  >
+                    <div style={{ fontWeight: "bold", fontSize: "1.4rem" }}>
+                      {p.toUpperCase()}
                     </div>
-                  </Col>
+                    <div
+                      style={{
+                        display: "flex",
+                        gap: "0.5rem",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {entities[p]?.map((pl) => (
+                        <Badge key={pl.tournamentId}>
+                          {getTournamentShortName(pl.tournamentId)}
+                        </Badge>
+                      ))}
+                    </div>
+                  </div>
+                </Col>
               );
             })}
           </Row>
@@ -83,3 +99,31 @@ export default function Players(): JSX.Element {
     </>
   );
 }
+
+const PlaceholderItem = () => {
+  return (
+    <Placeholder as={Col} xs={12} sm={6} md={4} animation="glow">
+      <Placeholder
+        as="div"
+        className="bg-black border border-secondary text-center p-2 m-2"
+        style={{ height: "4rem", width: "90%" }}
+      >
+        <div style={{ height: '80%' }} />
+      <div
+        style={{
+          display: "flex",
+          gap: "0.5rem",
+          justifyContent: "center",
+        }}
+      >
+        <Placeholder as={Badge}>
+          <div style={{ width: '2rem' }} />
+        </Placeholder>
+        <Placeholder as={Badge}>
+          <div style={{ width: '2rem' }} />
+        </Placeholder>
+      </div>
+      </Placeholder>
+    </Placeholder>
+  );
+};
