9638dfdac8045cefd97bb31731cd888e4b7ca654
Bomby K <bomby@flyntlok.com>
2022-03-19 22:41:06 -0800

    same origin

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:3001';
+const SERVER_PORT = 3001;
+export const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}:${SERVER_PORT}`;
